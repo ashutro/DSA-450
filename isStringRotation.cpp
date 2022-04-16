316e9298128bfb1f778f6dc6770b888b0ceff53b
Ashutosh Kumar <unknwnmex@gmail.com>
2022-04-16 23:35:35 +0530

    Checking two string is rotation to each other

---
+++ isStringRotation.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isRotationForm(string& s1 , string& s2){
+    if(s1.length() != s2.length()) return false;
+
+    string temp;
+
+    temp = s1+s2;
+    return(temp.find(s2) != string::npos);
+}
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        string s1;
+        cin>>s1;
+        string s2;
+        cin>>s2;
+        if(isRotationForm(s1,s2)){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    
+    return 0;
+}
