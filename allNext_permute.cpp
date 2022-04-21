1deb7945c7d3dae4cd67c6e9b22ad0f44017cfa3
Ashutosh Kumar <unknwnmex@gmail.com>
2022-04-21 12:23:53 +0530

    printing all permutation in a given array with common elements also

---
+++ allNext_permute.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define nl "\n"
+vector<vector<int>> ans;
+void Solver(vector<vector<int>> &ans, vector<int> v, int idx){
+    if(idx == v.size()){
+        ans.push_back(v);
+        return;
+    }
+    for(int i = idx; i < v.size();i++){
+        if(i != idx && v[i] == v[idx]) //logic to avoid the common elements swap:
+            continue;
+        swap(v[i],v[idx]);
+        Solver(ans,v,idx+1);
+    }
+    return;
+
+}
+vector<vector<int>> permute(vector<int> v){
+    sort(v.begin(),v.end());
+    vector<vector<int>> ans;
+    Solver(ans,v,0);
+
+    return ans;
+}
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        vector<int> v(n);
+        for(auto &i : v){
+            cin>>i;
+        }
+        vector<vector<int>> ans = permute(v);
+        for(auto i:ans){
+            for(auto j : i){
+                cout<<j<<" ";
+            }
+            cout<<nl;
+        }
+    }
+    
+    return 0;
+}
